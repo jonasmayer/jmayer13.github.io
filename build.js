c7198246b8588c5983d28f13639e98f7617f281f
Jonas <jmayer13@hotmail.com>
2016-10-05 20:15:19 -0300

    Add layouts

--- build.js
+++ build.js
@@ -1,10 +1,27 @@
 var Metalsmith = require('metalsmith');
-var amrkdown = require('metalsmith-markdown');
+var markdown = require('metalsmith-markdown');
+var layouts = require('metalsmith-layouts');
+var watch = require('metalsmith-watch');
 
 
 Metalsmith(__dirname)
 	.use(markdown())
+	.use(
+		watch({
+			paths: {
+				"${source}/**/*": true,
+				"${source}/**/**/*": true,
+			},
+			livereload: true,
+		})
+	)
+	.use(layouts({
+		"engine" : "handlebars",
+		"directory" : "layouts",
+		"partials": "layouts/partials"
+	}))
 	.build(function(err) {
 		if (err) throw err;
+		console.log("DONE!");
 	});
 
