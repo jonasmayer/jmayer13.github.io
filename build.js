a16e4d9e480cbee85bb65afcc28316c556e1e686
Jonas <jmayer13@hotmail.com>
2016-10-12 20:54:11 -0300

    Include 404 page to build

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ var layouts = require('metalsmith-layouts');
 var watch = require('metalsmith-watch');
 var sass = require('metalsmith-sass');
 var collections = require('metalsmith-collections');
-
+var assets = require('metalsmith-assets');
 
 Metalsmith(__dirname)
 	.use(collections({
@@ -28,6 +28,10 @@ Metalsmith(__dirname)
 	.use(sass({		
 		outputDir: 'css/',
 		outputStyle: "compressed"
+	}))
+	.use(assets({
+		source: './static',
+		destination: './'
 	}))
 	.build(function(err) {
 		if (err) throw err;
