a869d214e5f1be81aa5abaa6063b85584319b2b8
Jonas <jmayer13@hotmail.com>
2016-10-12 19:58:20 -0300

    Include collections and sass

--- build.js
+++ build.js
@@ -2,10 +2,14 @@ var Metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 var layouts = require('metalsmith-layouts');
 var watch = require('metalsmith-watch');
+var sass = require('metalsmith-sass');
+var collections = require('metalsmith-collections');
 
 
 Metalsmith(__dirname)
-	.use(markdown())
+	.use(collections({
+		pages: '*.md'
+	}))
 	.use(
 		watch({
 			paths: {
@@ -15,10 +19,15 @@ Metalsmith(__dirname)
 			livereload: true,
 		})
 	)
+	.use(markdown())
 	.use(layouts({
 		"engine" : "handlebars",
 		"directory" : "layouts",
 		"partials": "layouts/partials"
+	}))
+	.use(sass({		
+		outputDir: 'css/',
+		outputStyle: "compressed"
 	}))
 	.build(function(err) {
 		if (err) throw err;
